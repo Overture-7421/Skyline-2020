95706265085fef8d7c00360e11e9c10b66b0634b
Santiago Qunitana Moreno <a01571222@itesm.mx>
2020-01-10 20:53:54 -0600

    WORKS

--- src/subsystems/Chassis2.h
+++ src/subsystems/Chassis2.h
@@ -5,46 +5,40 @@
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/SpeedControllerGroup.h>
 #include <frc/smartdashboard/SmartDashboard.h>
-#include <AHRS.h>
+#include <AHRS.H>
 #include <frc/Encoder.h>
-#include <frc/kinematics/DifferentialDriveOdometry.h>
-#include <frc/kinematics/DifferentialDriveKinematics.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 #include "RobotMap.h"
+#include <frc/kinematics/DifferentialDriveKinematics.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
 
 using namespace ctre::phoenix::motorcontrol::can;
 using namespace ctre::phoenix::motorcontrol;
 
-class Chassis2 : public frc2::SubsystemBase {
- public:
-  Chassis2();
-  void Periodic() override;
-  void UpdateBoard();
-  void ArcadeDrive(double linear, double angular, bool squareInputs = true);
-  void TankDrive(double leftSpeed, double rightSpeed, bool squareInputs = true);
-  void updateSmartDashboard();
-  void getYaw();
+class Chassis2 : public frc2::SubsystemBase{
+    public:
+    Chassis2();
+    void Periodic() override;
+    void TankDrive(double leftSpeed, double rightSpeed, bool squareInputs=true);
+    void ArcadeDrive(double xSpeed, double 	zRotation, bool squareInputs=true);
+    double getYaw();
 
- private:
-  WPI_VictorSPX leftMotor {1};
-  WPI_VictorSPX rightMotor {2};
-  WPI_VictorSPX leftMotor1 {3};
-  WPI_VictorSPX rightMotor1 {4};
-  
+    private:
+    WPI_VictorSPX leftMotor  {ChassisMap::LEFTM};
+    WPI_VictorSPX rightMotor {ChassisMap::RIGHTM};
+    WPI_VictorSPX leftMotor1  {ChassisMap::LEFTM1};
+    WPI_VictorSPX rightMotor1{ChassisMap::RIGHTM1};
+    
+    frc::Encoder leftEncoder{ChassisMap::ENC_L_A,ChassisMap::ENC_L_B};
+    frc::Encoder rightEncoder{ChassisMap::ENC_R_A,ChassisMap::ENC_R_B};
+    frc::SpeedControllerGroup left{leftMotor, leftMotor1};
+    frc::SpeedControllerGroup right{rightMotor, rightMotor1};
+    frc::DifferentialDrive differentialDrive{left, right};
+    
+    double linear = 0;
+    double angular = 0;
 
-  frc::Encoder leftEncoder{ChassisMap::ENC_L_A,ChassisMap::ENC_L_B};
-  frc::Encoder rightEncoder{ChassisMap::ENC_R_A,ChassisMap::ENC_R_B};
-  frc::SpeedControllerGroup left{leftMotor, leftMotor1};
-  frc::SpeedControllerGroup right{rightMotor, rightMotor1};
-  frc::DifferentialDrive differentialDrive{left, right};
-
-  double linear = 0;
-  double angular = 0;
-  
-  AHRS gyro {SPI::Port::kMXP};
-  frc::DifferentialDriveOdometry odometry{frc::Rotation2d (units::degree_t(-gyro.GetAngle()));
-  frc::Pose2d pose;
-
-  DifferentialDriveKinematics::kinematics{units::meter_t(0.5)};
-  
+    AHRS gyro{SPI::Port::kMXP};
+    frc::DifferentialDriveOdometry odometry{frc::Rotation2d (units::degree_t(-gyro.GetAngle()))};
+    frc::Pose2d pose;
 };
-
