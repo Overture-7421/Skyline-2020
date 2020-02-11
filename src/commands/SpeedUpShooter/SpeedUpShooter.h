d6317ace650e282262cc5fb42fa7c87302f905ec
Santiago Qunitana Moreno <a01571222@itesm.mx>
2020-02-10 18:07:30 -0600

    New Shooter Command

---
+++ src/commands/SpeedUpShooter/SpeedUpShooter.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <subsystems/Shooter/Shooter.h>
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+class SpeedUpShooter
+    : public frc2::CommandHelper<frc2::CommandBase, SpeedUpShooter> {
+ public:
+
+  explicit SpeedUpShooter(Shooter* Shooter, double targetRPS);
+
+  void Initialize() override;
+
+  void Execute() override;
+
+  void End(bool interrupted) override;
+
+  bool IsFinished() override;
+
+  private:
+  Shooter* shooter;
+  double targetRPS;
+
+
+
+
+};
